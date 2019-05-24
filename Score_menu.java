fd06998f14949b492fbfe86c3aeb9e5235477ff7
Qipeipei <1033493603@qq.com>
2019-05-24 10:58:15 +0800

    change

---
+++ Score_menu.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+public class Score_menu extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public Score_menu() {
+//		JFrame jFrame = new JFrame("Rank List");
+//		Container container = jFrame.getContentPane();
+//		JLabel label = new JLabel("Rank list");
+//		container.setBackground(Color.black);
+		
+//		label.setHorizontalAlignment(SwingConstants.CENTER);
+//		container.add(label);
+		
+//		jFrame.setSize(480, 480);
+//		jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+//		this.addKeyListener(new MyKeyEvent());
+//		jFrame.setVisible(true);
+	}
+}
